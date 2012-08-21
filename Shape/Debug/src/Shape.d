d8a1f0e331d882af48ed392493ec4699d2f56c21
Bruno Guaman <bguaman@EZEQUIEL.(none)>
2012-08-21 14:43:22 -0500

    Primer Commit

---
+++ Shape/Debug/src/Shape.d
@@ -0,0 +1,12 @@
+src/Shape.d src/Shape.o: ../src/Shape.cpp \
+ /usr/local/include/opencv2/opencv.hpp ../src/storage_adaptors.hpp \
+ /usr/lib/jvm/java-6-sun-1.6.0.26/include/jni.h \
+ /usr/lib/jvm/java-6-sun-1.6.0.26/include/linux/jni_md.h
+
+/usr/local/include/opencv2/opencv.hpp:
+
+../src/storage_adaptors.hpp:
+
+/usr/lib/jvm/java-6-sun-1.6.0.26/include/jni.h:
+
+/usr/lib/jvm/java-6-sun-1.6.0.26/include/linux/jni_md.h:
---
+++ Shape/Release/objects.mk
@@ -0,0 +1,7 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_highgui -lopencv_core -lopencv_imgproc -lopencv_video -lopencv_ml -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_flann -lopencv_legacy
